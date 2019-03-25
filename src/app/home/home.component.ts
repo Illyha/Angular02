6813e0d39e8efda5b25e3a9b30d347c05d94f0c5
Ілля Слівчук <illyasliva433456@ukr.net>
2019-03-25 21:14:08 +0200

    changes added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  private users:object;
 
-  constructor() { }
+  constructor(private data:UsersService) { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.data.getUsers().subscribe( data=> {
+      console.log('We have new data!!!');
+      this.users = data;
+    });
+  }
+
+  firstClick(){
+    this.data.firstClick();
+    //console.log("Hello from first clickable button!!!");
   }
 
 }
